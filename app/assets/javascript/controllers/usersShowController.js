adb7a50a6ebf7191a63838c3af4af25255db02bc
split750 <godefroy.lemerdy@gmail.com>
2015-07-07 14:25:00 +0200

    change plantName

--- app/assets/javascript/controllers/usersShowController.js
+++ app/assets/javascript/controllers/usersShowController.js
@@ -1,9 +1,12 @@
 angular.module('NoteWrangler').controller('UsersShowController', function(User, Note, $scope, $routeParams){
   $scope.user = User.get({id: $routeParams.id});
-  
-  console.log($scope.user);
 
-  $scope.plantName = Note.get({_id: $scope.user.attachment});
+  $scope.user.$promise.then(function(data) {
+        
+    Note.get({id: data.attachment}, function(data2) {
+      $scope.plantName = data2.title;
+    });
 
-  console.log($scope.plantName);
+  });
+
 });
