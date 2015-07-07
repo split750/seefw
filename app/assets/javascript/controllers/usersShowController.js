07a3fb0aa26e8ab1fcaec963afb57902390c6928
split750 <godefroy.lemerdy@gmail.com>
2015-07-07 14:21:37 +0200

    test1

--- app/assets/javascript/controllers/usersShowController.js
+++ app/assets/javascript/controllers/usersShowController.js
@@ -1,7 +1,7 @@
 angular.module('NoteWrangler').controller('UsersShowController', function(User, Note, $scope, $routeParams){
   $scope.user = User.get({id: $routeParams.id});
   
-  console.log($scope.user.attachment);
+  console.log($scope.user);
 
   $scope.plantName = Note.get({_id: $scope.user.attachment});
 
