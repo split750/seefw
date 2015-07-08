74387ba0a37aa4f54990782712e9754f9f1620d3
split750 <godefroy.lemerdy@gmail.com>
2015-07-08 23:37:51 +0200

    add css

--- app/assets/javascript/controllers/notesIndexController.js
+++ app/assets/javascript/controllers/notesIndexController.js
@@ -1,8 +1,23 @@
-angular.module('NoteWrangler').controller('NotesIndexController', function(Note, Category, $scope){
+angular.module('NoteWrangler').controller('NotesIndexController', function(Note, Category, $scope, $log){
   
   $scope.notes = Note.query();
   $scope.search = {};
 
   $scope.categoryName = Category.query();
+
+  $scope.totalItems = $scope.notes.length;
+  $scope.currentPage = 1;
+
+  $scope.setPage = function (pageNo) {
+    $scope.currentPage = pageNo;
+  };
+
+  $scope.pageChanged = function() {
+    $log.log('Page changed to: ' + $scope.currentPage);
+  };
+
+  $scope.maxSize = 1;
+  $scope.bigTotalItems = 175;
+  $scope.bigCurrentPage = 1;
 
 });
