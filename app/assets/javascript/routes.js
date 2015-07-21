6b79ab7fe117143f6d8b5e9b008829fe16dbce9f
split750 <godefroy.lemerdy@gmail.com>
2015-07-21 10:34:13 +0200

    add login 3

--- app/assets/javascript/routes.js
+++ app/assets/javascript/routes.js
@@ -6,50 +6,57 @@ angular.module('NoteWrangler').config(function($routeProvider){
 
     .when('/notes', {
       templateUrl: "assets/templates/notes/index.html",
-      controller: "NotesIndexController"
+      controller: "NotesIndexController",
+      access: {restricted: true}
     })
 
     .when('/notes/new', {
       templateUrl: "assets/templates/notes/new.html",
-      controller: "NotesCreateController"
+      controller: "NotesCreateController",
+      access: {restricted: true}
     })
 
     .when('/notes/:id', {
       templateUrl: "assets/templates/notes/show.html",
-      controller: "NotesShowController"
+      controller: "NotesShowController",
+      access: {restricted: true}
     })
 
     .when('/notes/:id/edit', {
       templateUrl: "assets/templates/notes/edit.html",
-      controller: "NotesEditController"
+      controller: "NotesEditController",
+      access: {restricted: true}
     })
 
     .when('/users', {
       templateUrl: "assets/templates/users/index.html",
-      controller: "UsersIndexController"
+      controller: "UsersIndexController",
+      access: {restricted: true}
     })
 
     .when('/users/new', {
       templateUrl: "assets/templates/users/new.html",
-      controller: "UsersCreateController"
+      controller: "UsersCreateController",
+      access: {restricted: true}
     })
 
     .when('/users/:id', {
       templateUrl: "assets/templates/users/show.html",
-      controller: "UsersShowController"
+      controller: "UsersShowController",
+      access: {restricted: true}
     })
 
     .when('/users/:id/edit', {
       templateUrl: "assets/templates/users/edit.html",
-      controller: "UsersEditController"
+      controller: "UsersEditController",
+      access: {restricted: true}
     })
 
     .when('/about', {
       templateUrl: "assets/templates/about.html",
-      //controller: "AboutController"
+      //controller: "AboutController",
+      access: {restricted: true}
     })
-
-    .when('/login', {templateUrl: 'assets/templates/login.html'})
 
     .when('/login', {
       templateUrl: 'assets/templates/login.html', 
