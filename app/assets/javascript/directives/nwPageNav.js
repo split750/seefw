97bf6a2fe71d465a3b07f5d59f93e8f36c22ab42
split750 <godefroy.lemerdy@gmail.com>
2015-07-23 14:30:05 +0200

    change navigation top 2

--- app/assets/javascript/directives/nwPageNav.js
+++ app/assets/javascript/directives/nwPageNav.js
@@ -3,10 +3,12 @@ angular.module("NoteWrangler").directive('nwPageNav', function(){
     replace: true,
     restrict: "E",
     templateUrl: "assets/templates/directives/nwPageNav.html",
-    controller: function($scope, $location){
+    controller: function($scope, $location, AuthService){
       $scope.isPage = function(name){
         return new RegExp("/" + name + "($|/)").test($location.path());
       };
+
+      $scope.islogged = AuthService.getUserStatus();
     }
   };
 });
